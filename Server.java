950d9577939e455e3483d40c58f8a658311ba989
Wamique Abro <wamik.abro212@gmail.com>
2023-10-28 17:26:19 +0500

    Implement Server

---
+++ Server.java
@@ -0,0 +1,48 @@
+package ConsoleChatAppUsingSocket;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    ServerSocket serverSocket;
+
+    Server(ServerSocket serverSocket){
+        this.serverSocket = serverSocket;
+    }
+
+    public void startServer(){
+        try{
+            while(!serverSocket.isClosed()){
+                Socket socket = serverSocket.accept();
+                System.out.println("A new client has connected.");
+                ClientHandler clientHandler = new ClientHandler(socket);
+
+                Thread thread = new Thread(clientHandler);
+                thread.start();
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void closeServerSocket(){
+        try{
+            if(serverSocket != null)
+                serverSocket.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(5555);
+            Server server = new Server(serverSocket);
+            server.startServer();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
